f73f90e311a947e77e0ce2f4e56cd5c98b9d1271
Eskil J <eskil@vuzii.com>
2017-03-20 15:50:02 +0000

    find2

--- cw_4/find.py
+++ cw_4/find.py
@@ -12,13 +12,13 @@ fig = plt.figure(figsize=(12, 6))
 ax1 = fig.add_subplot(1, 2, 1)
 ax2 = fig.add_subplot(1, 2, 2)
 
-for root, dirs, files in os.walk("../notebooks/tf-log/ac"):
+for root, dirs, files in os.walk("../notebooks/tf-log/filter"):
     for file in files:
         if file.endswith(".npz"):
             path = os.path.join(root, file)
             data = np.load(path)
             final_acc = data['valid_accuracy'][-1]
-            if 'epochs=15' in path:
+            if 'epochs=5' in path:
                 plot_data2 = data['valid_accuracy']
                 plot_data = data['train_accuracy']
                 ax1.plot(np.arange(1, plot_data.shape[0] + 1), plot_data, label=str(path.split('ac=', 1)[1].split(',',1)[0] + ', lr=' + path.split('lr=', 1)[1].split(',',1)[0]))
