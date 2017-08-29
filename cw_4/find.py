6d78af06a82b8dd38b937aa9c4448d2811e1d13a
Eskil J <eskil@vuzii.com>
2017-08-29 18:05:12 +0200

    removed data-directory from git

--- cw_4/find.py
+++ cw_4/find.py
@@ -13,7 +13,7 @@ fig = plt.figure(figsize=(12, 6))
 ax1 = fig.add_subplot(1, 2, 1)
 ax2 = fig.add_subplot(1, 2, 2)
 
-for root, dirs, files in os.walk("../notebooks/tf-log/filter"):
+for root, dirs, files in os.walk("../notebooks/tf-log/final3"):
     for file in files:
         if file.endswith(".npz"):
             path = os.path.join(root, file)
@@ -21,19 +21,22 @@ for root, dirs, files in os.walk("../notebooks/tf-log/filter"):
             data = np.load(path)
             final_acc = data['valid_error'][-1]
             # if 'epochs=5' in path:
-            #     plot_data = data['train_error']
-            #     plot_data2 = np.subtract(plot_data, data['valid_error'])
-            #     ax1.plot(np.arange(1, plot_data.shape[0] + 1), plot_data, label=str(path.split('wd=', 1)[1].split(',',1)[0] + ', bn=' + path.split('bn=', 1)[1].split(',',1)[0]) + ', do=' + path.split('do=', 1)[1].split(',',1)[0])
-            #     ax2.plot(np.arange(1, plot_data2.shape[0] + 1), plot_data2)
+            #     if 'ac=elu,fs=3,nf=32,lr=0.0004,epochs=5,wd=0.0005,do=1.0,bn=False' not in path:
+            #         plot_data = data['train_accuracy']
+            #         plot_data2 = np.subtract(plot_data, data['valid_accuracy'])
+            #         ax1.plot(np.arange(1, plot_data.shape[0] + 1), plot_data, label=str(path.split('wd=', 1)[1].split(',',1)[0] + ', bn=' + path.split('bn=', 1)[1].split(',',1)[0]) + ', do=' + path.split('do=', 1)[1].split(',',1)[0])
+            #         ax2.plot(np.arange(1, plot_data2.shape[0] + 1), plot_data2)
+            plot_data = data['train_accuracy']
+            plot_data2 = data['valid_accuracy']
 
-            if 'fs=3,nf=32' in path:
+            print(plot_data)
 
-                plot_data = data['train_error']
-                plot_data2 = data['valid_error']
-                print(plot_data)
-                print(plot_data2)
-                ax1.plot(plot_data, label=str('fs=' + path.split('fs=', 1)[1].split(',',1)[0]) + ', nf=' + path.split('nf=', 1)[1].split(',',1)[0])
-                ax2.plot(plot_data2)
+            if '2' in path:
+                ax1.plot(plot_data, label='Training Set')
+                ax1.plot(plot_data2, label='Validation Set')
+            else:
+                ax2.plot(plot_data, label='Training Set')
+                ax2.plot(plot_data2, label='Validation Set')
 
             if final_acc > best_acc:
                 if best_acc2 != 0:
@@ -51,8 +54,8 @@ print('2nd Best acc: {}, by run {}'.format(best_acc2, best_run2))
 
 ax1.legend(loc=0 )
 ax1.set_xlabel('Epoch number')
-ax1.set_ylabel('Training set Error')
+ax1.set_ylabel('Accuracy')
 ax2.set_xlabel('Epoch number')
-ax2.set_ylabel('Validation set Error')
+ax2.set_ylabel('Accuracy')
 
 fig.savefig('new_fig_error.pdf')
